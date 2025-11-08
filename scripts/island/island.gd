aebca11ba95aebae974c2f2da9df4f1bec28c083
brangarten <brandonjgarcia1024@gmail.com>
2025-11-08 02:28:35 -0500

    Progress: Created Island class, Added background, Added Wallet Todo: - Get node reference for wallet label, and implement a first island with different states (unlocked & locked)

---
+++ scripts/island/island.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name Island
+
+@export var island_name = "Campus"
+@export var island_unlocked = false
+@export var population : int = 1
+@export var items : Dictionary = {
+	"Item1" : {
+		"Name": "Blank",
+		"Value": 1.00,
+		"Description": "Desc Maou",
+		"Locked": false
+	},
+	"Item2" : {
+		"Name": "Blank",
+		"Value": 1.00,
+		"Description": "Desc Maou",
+		"Locked": true 
+	},
+	"Item3" : {
+		"Name": "Blank",
+		"Value": 1.00,
+		"Description": "Desc Maou", 
+		"Locked": true
+	}
+}
+
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
 class_name PlayerClass
 
-const SPEED = 500
+const SPEED : int = 500
+
+func _add_val(event):
+	"""
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			Wallet.wallet_value += 2.25"""
+	pass
 
 func _get_input():
 	var input_direction = Input.get_vector("arrowkey_left", "arrowkey_right", "arrowkey_up", "arrowkey_down", 0.2)
@@ -10,5 +17,6 @@ func _get_input():
 
 func _physics_process(delta: float) -> void:
 	_get_input()
+	_add_val(Input)
 	move_and_slide()
 	
